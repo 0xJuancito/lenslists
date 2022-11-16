f9ada1ac668a6070921fd1d541f01716c2f8ce10
juancito.eth <juancitoeth@gmail.com>
2022-11-16 10:00:22 -0300

    replace error messages

--- pages/api/lists/index.ts
+++ pages/api/lists/index.ts
@@ -10,7 +10,7 @@ export default async function handler(
   res: NextApiResponse<ListResponse | ErrorResponse>,
 ) {
   if (!['POST'].includes(req.method as string)) {
-    return res.status(404).json({ message: 'Endpoint not found.' });
+    return res.status(404).json({ message: 'Method not allowed.' });
   }
 
   const token = req.headers['x-access-token'] as string;
