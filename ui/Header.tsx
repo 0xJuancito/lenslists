acdcf327c6bba8c003e8a5a8c5cbd3ab99706306
juancito.eth <juancitoeth@gmail.com>
2022-11-17 14:16:34 -0300

    update logo

--- ui/Header.tsx
+++ ui/Header.tsx
@@ -4,16 +4,15 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className="item flex h-20 w-full border-b bg-white">
-      <div className="container mx-auto flex max-w-screen-xl justify-center px-4">
+    <div className="item flex h-16 w-full border-b bg-white">
+      <div className="container mx-auto flex max-w-screen-xl px-4">
         <div className="flex items-center gap-3">
           <Image
-            className="rounded-xl bg-zinc-100"
             src="/lenslists.png"
             unoptimized
             alt="Lens Lists Logo"
-            width={220}
-            height={38}
+            width={174}
+            height={30}
           />
           {/* <span className="font-semibold text-zinc-800">Lens Lists</span> */}
         </div>
