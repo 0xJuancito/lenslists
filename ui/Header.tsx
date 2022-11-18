dc3982141ea8fb37418362087bfb0ae62c2ae9db
juancito.eth <juancitoeth@gmail.com>
2022-11-17 21:40:20 -0300

    add create list button

--- ui/Header.tsx
+++ ui/Header.tsx
@@ -19,7 +19,21 @@ export default function Header() {
             />
           </div>
         </Link>
-        <LoginButton></LoginButton>
+        <div className="flex gap-2 sm:gap-4">
+          <button className="flex cursor-pointer items-center gap-2 rounded-2xl bg-sky-700 px-4 py-2 text-white shadow-md hover:bg-sky-800">
+            <svg
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              className="h-4 w-4 fill-white font-bold"
+            >
+              <g>
+                <path d="M5.5 4c-.28 0-.5.22-.5.5v15c0 .28.22.5.5.5H12v2H5.5C4.12 22 3 20.88 3 19.5v-15C3 3.12 4.12 2 5.5 2h13C19.88 2 21 3.12 21 4.5V13h-2V4.5c0-.28-.22-.5-.5-.5h-13zM16 10H8V8h8v2zm-8 2h8v2H8v-2zm10 7v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z"></path>
+              </g>
+            </svg>
+            <span className="hidden sm:block">CREATE LIST</span>
+          </button>
+          <LoginButton></LoginButton>
+        </div>
       </div>
     </div>
   );
