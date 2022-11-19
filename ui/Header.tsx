15f7ccffb56025ddaea4ba96edc09c8181fc2fca
juancito.eth <juancitoeth@gmail.com>
2022-11-19 02:33:20 -0300

    improve styling

--- ui/Header.tsx
+++ ui/Header.tsx
@@ -22,14 +22,17 @@ export default function Header() {
               width={40}
               height={40}
             />
-            <span className="hidden text-3xl font-bold text-black sm:inline-block">
+            <span className="flex text-3xl font-bold text-black">
               LENS LISTS
             </span>
           </div>
         </Link>
-        <div className="flex gap-2 sm:gap-4">
+        <div className="flex gap-2 sm:gap-8">
+          <div className="hidden cursor-pointer items-center text-lg font-bold text-black sm:flex">
+            MY LISTS
+          </div>
           <button
-            className="flex cursor-pointer items-center gap-2 rounded-2xl bg-sky-600 px-4 py-2 text-white shadow-md hover:bg-sky-700"
+            className="hidden cursor-pointer items-center gap-2 rounded-2xl bg-sky-600 px-4 py-2 text-white shadow-md hover:bg-sky-700 sm:flex"
             onClick={() => {
               setShowListModal(true);
             }}
@@ -43,7 +46,7 @@ export default function Header() {
                 <path d="M5.5 4c-.28 0-.5.22-.5.5v15c0 .28.22.5.5.5H12v2H5.5C4.12 22 3 20.88 3 19.5v-15C3 3.12 4.12 2 5.5 2h13C19.88 2 21 3.12 21 4.5V13h-2V4.5c0-.28-.22-.5-.5-.5h-13zM16 10H8V8h8v2zm-8 2h8v2H8v-2zm10 7v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z"></path>
               </g>
             </svg>
-            <span className="hidden sm:block">CREATE LIST</span>
+            <span>CREATE LIST</span>
           </button>
           {showListModal && (
             <ListModal
