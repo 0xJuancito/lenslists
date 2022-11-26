d2ea0affd515a49698cb4af4ddb33037ef4b3021
juancito.eth <juancitoeth@gmail.com>
2022-11-26 03:04:48 -0300

    fix col grids

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
       {loading ? (
         <Loading></Loading>
       ) : (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {cards.map((card, index) => (
             <ListCard key={index} {...card}></ListCard>
           ))}
