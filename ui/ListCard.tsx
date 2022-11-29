82f55230979535f7e706ad762bce2427f2577cc6
juancito.eth <juancitoeth@gmail.com>
2022-11-29 18:34:23 -0300

    remove follow button in lists

--- ui/ListCard.tsx
+++ ui/ListCard.tsx
@@ -134,12 +134,13 @@ export default function ListCard({
               EDIT LIST
             </button>
           ) : (
-            <button
-              onClick={(e) => e.stopPropagation()}
-              className="mt-2 mb-1 cursor-pointer rounded-2xl bg-lens-lime px-4 py-2 text-black shadow-md hover:bg-lens-lime-hover"
-            >
-              FOLLOW
-            </button>
+            ''
+            // <button
+            //   onClick={(e) => e.stopPropagation()}
+            //   className="mt-2 mb-1 cursor-pointer rounded-2xl bg-lens-lime px-4 py-2 text-black shadow-md hover:bg-lens-lime-hover"
+            // >
+            //   FOLLOW
+            // </button>
           )}
         </div>
       </div>
