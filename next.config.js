410e5b5d651567d61125acafec54fda506ff3a8a
juancito.eth <juancitoeth@gmail.com>
2022-12-01 17:30:03 -0300

    remove transpiled packages

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
-    transpilePackages: [
-      'react-syntax-highlighter',
-      'swagger-client',
-      'swagger-ui-react',
-    ],
   },
 };
 
