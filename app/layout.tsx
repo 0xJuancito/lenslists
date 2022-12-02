b2b36b9f1d9fb0e8e649cd95bbeaae24f39dbee0
juancito.eth <juancitoeth@gmail.com>
2022-12-02 00:51:47 -0300

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Lens Lists</title>
-
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
